0dfd2ce67a91807fbed186baf8c55aa95a65276f
glo6al <towers.a@gmail.com>
2022-05-09 12:41:38 -0400

    add html content, style, and declare variables

---
+++ assets/JS/index.js
@@ -0,0 +1,10 @@
+// declare variabls
+
+var questionIndex = 0;
+var timerEl = document.querySelector("#startTimer");
+var timeLeft = document.querySelector("#timer");
+var quizDisplay = document.querySelector("#questionBox");
+var containerBox = document.querySelector("#container");
+var timeStartCount = 75;
+var timeStops = 0;
+var createList = document.createElement("ul");
